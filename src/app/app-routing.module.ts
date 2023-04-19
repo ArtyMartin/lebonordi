f1729c943af8cd963d2bf809e917e1473903a9fc
ArtyMartin <arty03721@gmail.com>
2023-04-19 16:09:45 +0200

    questionnaire v2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { PropositionComponent } from './proposition/proposition.component';
+import { QuestionnaireComponent } from './questionnaire/questionnaire.component';
 
 const routes: Routes = [
   {path: '', component: HeaderComponent},
-   {path: 'propositions', component: PropositionComponent }
+   {path: 'propositions', component: PropositionComponent },
+   {path: 'questionaire', component: QuestionnaireComponent }
 ];
 
 @NgModule({
