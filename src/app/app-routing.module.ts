6efe196f57a2546b498673011826028b2f0a24de
charafeloua <ysfelmokhtar@gmail.com>
2023-04-19 15:57:59 +0200

    add proposition component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { PropositionComponent } from './proposition/proposition.component';
 
 const routes: Routes = [
-  {path: '', component: HeaderComponent}
+  {path: '', component: HeaderComponent},
+   {path: 'propositions', component: PropositionComponent }
 ];
 
 @NgModule({
---
+++ src/app/proposition/proposition.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-proposition',
+  templateUrl: './proposition.component.html',
+  styleUrls: ['./proposition.component.css']
+})
+export class PropositionComponent {
+
+}
