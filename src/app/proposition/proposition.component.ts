eaf7587b4e2afb8fda26266757ce565a419c9579
charafeloua <ysfelmokhtar@gmail.com>
2023-05-13 09:56:39 +0200

    proposition page

--- src/app/proposition/proposition.component.ts
+++ src/app/proposition/proposition.component.ts
@@ -1,31 +1,18 @@
 import { Component } from '@angular/core';
+import ComputerJson from '../computer.json';
+
+interface COMPUTER {
+nom: string;
+prix: number;
+image: string;
+}
+
+
 @Component({
   selector: 'app-proposition',
   templateUrl: './proposition.component.html',
   styleUrls: ['./proposition.component.css'],
 })
 export class PropositionComponent {
-  computers = [
-    {
-      nom: 'Ordinateur 1',
-      prix: 999,
-      image: 'chemin'
-    },
-    {
-      nom: 'Ordinateur 2',
-      prix: 1499,
-      image: 'chemin'
-    },
-    {
-      nom: 'Ordinateur 3',
-      prix: 1500,
-      image: 'chemin'
-    },
-    {
-      nom: 'Ordinateur 4',
-      prix: 1500,
-      image: 'chemin'
-    }
-    // Ajoutez d'autres ordinateurs ici
-  ];
+  computer: COMPUTER[] = ComputerJson;
 }
