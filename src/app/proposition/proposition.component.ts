c6b69369768f074e06bbc83f840f954a4d3d1b92
charafeloua <ysfelmokhtar@gmail.com>
2023-05-13 09:16:10 +0200

    proposition page

--- src/app/proposition/proposition.component.ts
+++ src/app/proposition/proposition.component.ts
@@ -5,7 +5,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./proposition.component.css'],
 })
 export class PropositionComponent {
-  titre!: string;
-  prix!: number;
-  imageUrl!: string;
+  computers = [
+    {
+      nom: 'Ordinateur 1',
+      prix: 999,
+      image: 'chemin'
+    },
+    {
+      nom: 'Ordinateur 2',
+      prix: 1499,
+      image: 'chemin'
+    },
+    {
+      nom: 'Ordinateur 3',
+      prix: 1500,
+      image: 'chemin'
+    },
+    {
+      nom: 'Ordinateur 4',
+      prix: 1500,
+      image: 'chemin'
+    }
+    // Ajoutez d'autres ordinateurs ici
+  ];
 }
