80bcb651a8203fa6170d92c6cab4ff0d11ba6d9e
ArtyMartin <arty03721@gmail.com>
2023-05-16 10:32:08 +0200

    import commun module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { PropositionComponent } from './proposition/proposition.component';
 import { HomeComponent } from './home/home.component';
+import { QuestionnaireComponent } from './questionnaire/questionnaire.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     PropositionComponent,
-    HomeComponent
+    HomeComponent,
+    QuestionnaireComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/questionnaire/questionnaire.component.ts
+++ src/app/questionnaire/questionnaire.component.ts
@@ -17,8 +17,30 @@ export class QuestionnaireComponent {
   public questionnaire: Questionnaire[] = [];
 
   constructor() {
-    const question: Questionnaire = {
+
+    const question1: Questionnaire = {
       number: 1,
+      question : "Voulez-vous un ordinateur fixe ou portable ?",
+      reponses: [
+        "un ordinateur fixe",
+        "un ordinateur portable",
+      ]
+    };
+    this.questionnaire.push(question1);
+
+    const question2: Questionnaire = {
+      number: 3,
+      question : "Voulez-vous un ordinateur pour réaliser des tâches de bureautique ?",
+      reponses: [
+        "Oui, c'est mon activité principal",
+        "Oui",
+        "Non"
+      ]
+    };
+    this.questionnaire.push(question2);
+
+    const question3: Questionnaire = {
+      number: 3,
       question : "Voulez-vous un ordinateur pour jouer aux jeux vidéos ?",
       reponses: [
         "Oui, avec des graphismes en élevé",
@@ -27,6 +49,36 @@ export class QuestionnaireComponent {
         "Non"
       ]
     };
-    this.questionnaire.push(question);
+    this.questionnaire.push(question3);
+
+
+    const question4: Questionnaire = {
+      number: 4,
+      question : "Voulez-vous un ordinateur pour réaliser de l’édition de photos / vidéos ?",
+      reponses: [
+        "Oui, pour les 2",
+        "Oui, mais seulement de la vidéo",
+        "Oui, mais seulement de la photo",
+        "Non"
+      ]
+    };
+    this.questionnaire.push(question4);
+
+
+    const question5: Questionnaire = {
+      number: 5,
+      question : "Voulez-vous un ordinateur pour stocker plein de fichiers (photos, vidéo, jeux vidéo) ?",
+      reponses: [
+        "Oui, c’est mon activité principal",
+        "Oui",
+        "Non"
+      ]
+    };
+    this.questionnaire.push(question5);
+
+    console.log(this.questionnaire[0]);
+    
+
+    
   }
 }
