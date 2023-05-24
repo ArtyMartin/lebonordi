4b0735ee32471f075338fd861558e4269a355425
rutra <contact@wooden-master.fr>
2023-05-24 22:16:59 +0200

    pas de coreection pour le type d'ordinateur

--- src/app/proposition/proposition.component.ts
+++ src/app/proposition/proposition.component.ts
@@ -92,12 +92,25 @@ export class PropositionComponent {
 
     const computers: Computer[] = ComputerJson;
 
+    console.log(this.isLaptop);
+
+    if (this.isLaptop) {
+      console.log("p");
+    } else {
+      console.log("f");
+    }
+    
+    
+    //console.log(this.isLaptop ? "p" : "f");
+    
+
     this.filteredComputers = computers
-      .filter((computer) => computer.type === (this.isLaptop?'fixe':'portable'))
-      .filter((computer) => computer.system.ram >= (this.cptRam*3) )
-      .filter((computer) => computer.system.cpu.core >= (this.cptProc) )
-      .filter((computer) => computer.system.cg.score >= (this.cptGpu*50) )
-      .filter((computer) => computer.system.hdd.capacite >= (this.cptHdd * 10))
+      .filter((computer) => computer.type === (this.isLaptop ? "fixe" : "portable"))
+      .filter((computer) => computer.system.ram >= (this.cptRam * 3))
+      .filter((computer) => computer.system.cpu.core >= this.cptProc)
+      .filter((computer) => computer.system.cg.score >= (this.cptGpu * 50))
+      .filter((computer) => computer.system.hdd.capacite >= (this.cptHdd * 10));
+
     
     
     console.log(this.filteredComputers);
