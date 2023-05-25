adeda87fd96042ba6678d6233eea05c059ef0ed0
charafeloua <ysfelmokhtar@gmail.com>
2023-05-26 00:05:32 +0200

    formDone

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -8,28 +8,37 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class ContactComponent {
   formulaire: FormGroup;
-
+  submitted = false;
 
   constructor(private formBuilder: FormBuilder) {
     this.formulaire = this.formBuilder.group({
       nom: ['', Validators.required],
       prenom: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
+      email: ['', Validators.required],
       sujet: ['', Validators.required],
-      contenu: ['', Validators.required]
+      contenu: ['', Validators.required],
     });
   }
+  get f() { return this.formulaire.controls; }
   
 
-  envoyerFormulaire() {
-    if (this.formulaire.valid) {
-      // Logique pour envoyer le formulaire
-  
-      // Redirection vers la page d'accueil
-      window.location.href = './home';
-    } else {
-      alert('Veuillez remplir correctement tous les champs');
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.formulaire.invalid) {
+      return;
     }
-  }
+
+    // display form values on success
+    setTimeout(() => {
+      window.location.href = './home';
+    }, 3000);
+}
+
+onReset() {
+    this.submitted = false;
+    this.formulaire.reset();
+}
 }
 
